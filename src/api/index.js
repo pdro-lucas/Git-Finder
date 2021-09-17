cc31bcd5408ac6100fc94e227933334ea2054764
pdro-lucas <pedroluckaz22@gmail.com>
2021-09-17 20:52:08 -0300

    feat: get user data with GitHub API

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,27 @@
 import axios from 'axios';
 
-export const getUsersData = async (url) => {
-  const { data } = await axios(
-    `${url}?client_id=${import.meta.env.VITE_CLIENT_ID}&client_secret=${import.meta.env.VITE_CLIENT_SECRET}`
-  );
-  return data;
-}
+export const getUsersData = async (user) => {
+  let url = 'https://api.github.com/users?per_page=8';
+
+  if (user) {
+    url = `https://api.github.com/users/${user}`;
+  }
+
+  // By default, Github only accepts 60 requests when the user is not
+  // autenticated. Import your personal acess token to make more requests.
+
+  try {
+    const { data } = await axios({
+      url,
+      headers: {
+        Accept: 'application/vnd.github.v3+json',
+        Authorization:
+          `token ${import.meta.env.VITE_PERSONAL_ACESS_TOKEN}` || null,
+      },
+    });
+
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+};
