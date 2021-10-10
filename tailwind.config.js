84adae84179df156329690da404ce1166df53349
pdro-lucas <pedroluckaz22@gmail.com>
2021-10-10 17:13:46 -0300

    :wrench: Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ['./index.html', './src/**/*.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-
+    backgroundColor: (theme) => ({
+      ...theme('colors'),
+      blueGray: '#141C2F',
+      blueGrayLight: '#1F2A48',
+    }),
   },
   variants: {
     extend: {},
